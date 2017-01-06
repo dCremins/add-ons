26f8ac3d99a4b84d943eb7691d75a0a129f34b2c
Devin Cremins <drcremin@ncsu.edu>
2017-01-06 14:20:35 -0500

    hodge podge

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ "$TRAVIS_BRANCH" != "development" ]; then
-    exit 0;
-fi
+if [ "$TRAVIS_BRANCH" == "development" ]; then
+
+echo -e "Generating Master...\n"
 
 export GIT_COMMITTER_EMAIL= "devincrem@gmail.com"
 export GIT_COMMITTER_NAME= "Travis CI"
@@ -10,3 +10,5 @@ export GIT_COMMITTER_NAME= "Travis CI"
 git checkout master || exit
 git merge "$TRAVIS_COMMIT" || exit
 git push "https://${DEPLOY_KEY}@$github.com/dCremins/add-ons.git" development:master
+
+fi
