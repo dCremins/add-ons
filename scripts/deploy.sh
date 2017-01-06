7c7e5190c0f8e85a6b64df57dcc7460cbd0bcaa3
Devin Cremins <drcremin@ncsu.edu>
2017-01-06 11:49:20 -0500

    new tutorial

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,29 @@
-if [ "${TRAVIS_BRANCH}" != "development" ]; then 
-    exit 0;
-fi
+#!/usr/bin/env ruby
 
-export GIT_COMMITTER_EMAIL=...
-export GIT_COMMITTER_NAME=...
+# HOWTO:
+# copy this file to script/travis-deploy
+# chmod +x script/travis-deploy
+# add the following to your .travis.yml
+# after_success:
+#  - "script/travis-deploy"
 
-git checkout master || exit
-git merge "$TRAVIS_COMMIT" || exit
-git push ... # here need some authorization and url
+
+exit if ENV["TRAVIS_DEPLOY_PUSHED"]
+
+# set defaults
+ENV["TRAVIS_DEPLOY_SOURCE_BRANCH"] ||= "development"
+ENV["TRAVIS_DEPLOY_TARGET_BRANCH"] ||= "master"
+
+# make sure we only deploy the correct branch
+if ENV["TRAVIS_BRANCH"] != ENV["TRAVIS_DEPLOY_SOURCE_BRANCH"] || ENV["TRAVIS_PULL_REQUEST"] != "false"
+  puts "no deployment for:  #{ENV["TRAVIS_BRANCH"]} pull request: #{ENV["TRAVIS_PULL_REQUEST"]} job number: #{ENV["TRAVIS_JOB_NUMBER"]}"
+  exit(true)
+end
+
+# push from the source branch to the master branch
+puts "pushing from #{ENV["TRAVIS_DEPLOY_SOURCE_BRANCH"]} to #{ENV["TRAVIS_DEPLOY_TARGET_BRANCH"]}"
+`git config user.name Choco`
+`git checkout #{ENV["TRAVIS_DEPLOY_SOURCE_BRANCH"]}`
+`git push origin #{ENV["TRAVIS_DEPLOY_SOURCE_BRANCH"]}:#{ENV["TRAVIS_DEPLOY_TARGET_BRANCH"]}`
+puts "DONE, have a great day!"
+ENV["TRAVIS_DEPLOY_PUSHED"] = "true"
