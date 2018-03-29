aa6e1d279a22b9a94cfe7f7273d223e19b138da2
Devin Cremins <drcremin@ncsu.edu>
2018-03-29 09:38:38 -0400

    search style tweaks and proper labels

--- widgets/search.php
+++ widgets/search.php
@@ -2,148 +2,107 @@
 
 namespace AddOn\Search;
 
-class ItreSearch extends \WP_Widget
-{
+class ItreSearch extends \WP_Widget {
+	public function __construct() {
+		$widget_details = array(
+			'classname' => 'ItreSearch',
+	  	'description' => 'Custom Research Search Widget'
+	  );
+		parent::__construct('ItreSearch', 'Search Research', $widget_details);
+	}
 
-// constructor
-    public function __construct()
-    {
-        $widget_details = array(
-        'classname' => 'ItreSearch',
-        'description' => 'Custom Research Search Widget'
-        );
+  public function form($instance) { }
 
-        parent::__construct('ItreSearch', 'Search Research', $widget_details);
-    }
+  public function update($new_instance, $old_instance) { }
 
-    // widget form creation
-    public function form($instance)
-    {
-        // Backend Form
-    }
+  public function widget($args, $instance) {
+		global $post, $post_ID, $wpdb;
+    $category = get_categories();
+    if (class_exists('acf')) {
+			$sql = $wpdb->prepare("select post_id from "
+				. $wpdb->prefix
+				. "postmeta where meta_key = %s limit 0,1 ", 'files');
+      $post = $wpdb->get_results($sql);
+			if ($post) {
+				if (get_field('files', $post[0]->post_id)) {
+					while (has_sub_field('files', $post[0]->post_id)) {
+						$fileType = get_sub_field_object('type', $post[0]->post_id);
+					}
+				}
+			}
+		} ?>
 
-    // widget update
-    public function update($new_instance, $old_instance)
-    {
-    }
+		<form method="post" class="search-form form-horizontal" id="advanced-searchform" role="search" action="<?php echo esc_url(home_url('/')); ?>">
+			<h3><?php _e('Advanced Search', 'textdomain'); ?></h3>
+			<input type="hidden" name="search" value="advanced">
+			<div class="input-group">
+				<label class="hidden" for="s"><?php _e('Keyword:'); ?></label>
+        <input type="search" placeholder="Keyword" class="form-control" name="s" id="s">
+      </div>
 
-    // widget display
-    public function widget($args, $instance)
-    {
-        global $post, $post_ID, $wpdb;
-        $category = get_categories();
-// Get File Type Options
-        if (class_exists('acf')) { // If ACF Installed
-// Dynamically get the Post ID of the first post
-            $sql = $wpdb->prepare("select post_id from " . $wpdb->prefix . "postmeta where meta_key = %s limit 0,1 ", 'files');
-            $post = $wpdb->get_results($sql);
-// Get the File Type sub field from the first post
-// Then store the field object into our variable
-            if ($post) {
-              if (get_field('files', $post[0]->post_id)) {
-                  while (has_sub_field('files', $post[0]->post_id)) {
-                      $fileType = get_sub_field_object('type', $post[0]->post_id);
-                  }
-              }
-            }
-        }
-        ?>
+			<?php if (class_exists( 'Bylines\Objects\Byline' )) {
+				$terms = get_terms('byline');?>
+				<div class="input-group">
+					<label for="authors" class=""><?php _e('Select an Author: ', 'textdomain'); ?></label><br>
+					<select name="authors" id="authors" class="form-control">
+						<optgroup label="Authors">
+							<option selected value="">All Authors</option>
+							<?php foreach ($terms as $byline) {
+								if ((get_user_by('ID', $byline->ID)) || get_user_by('slug', $byline->slug)) {
+										 //
+								} else { ?>
+									<option value="<?php echo $byline->slug; ?>"><?php echo $byline->name; ?></option>
+								<?php  }
+							}; ?>
+						</optgroup>
+					</select>
+				</div>
+			<?php }?>
 
-        <form method="post" class="search-form form-horizontal" id="advanced-searchform" role="search" action="<?php echo esc_url(home_url('/')); ?>">
-            <h3><?php _e('Advanced Search', 'textdomain'); ?></h3>
-            <input type="hidden" name="search" value="advanced">
-<!-- Keyword -->
-            <div class="input-group">
-                <label class="hidden" for="s"><?php _e('Keyword:'); ?></label>
-                <input type="search" placeholder="Keyword" class="form-control" name="s">
-            </div>
-<!-- /Keyword -->
+			<div class="input-group">
+				<label for="cat" class=""><?php _e('Select a Category: ', 'textdomain'); ?></label><br>
+				<select name="cat" id="cat" class="form-control">
+					<optgroup label="Categories">
+						<option selected value="">All Categories</option>
+						<?php foreach ($category as $cats) { ?>
+							<option value="<?php echo $cats->slug?>"><?php echo $cats->name; ?></option>
+						<?php }; ?>
+					</optgroup>
+				</select>
+			</div>
 
-<!-- Author -->
-					<?php if (class_exists( 'Bylines\Objects\Byline' )) {
-						 $terms = get_terms('byline');?>
-             <div class="input-group">
-	             <label for="authors" class=""><?php _e('Select an Author: ', 'textdomain'); ?></label><br>
-							 <select name="authors"  class="form-control">
-								 <optgroup label="Authors">
-									 <option selected value="">All Authors</option>
-									 <?php foreach ($terms as $byline) {
-										 if ((get_user_by('ID', $byline->ID)) || get_user_by('slug', $byline->slug)) {
-											 //
-											 echo 'nope';
-										 } else { ?>
-											 <option value="<?php echo $byline->slug; ?>"><?php echo $byline->name; ?></option>
-									 <?php  }
-									 ?>
-									 <?php }; ?>
-								 </optgroup>
-							 </select>
-					 	</div>
-					 <?php }?>
-<!-- /Author -->
+			<?php if (class_exists('acf') && get_field('files')) { ?>
+				<div class="input-group">
+					<label for="fileType" class=""><?php _e('Select a Document Type: ', 'textdomain'); ?></label><br>
+					<select name="fileType" id="fileType" class="form-control">
+						<option value="">All Types</option>
+            <?php foreach ($fileType['choices'] as $k => $v) {?>
+							<option value="<?php echo $k?>"><?php echo $v; ?></option>
+						<?php }; ?>
+					</select>
+				</div>
+			<?php }; ?>
 
-<!-- Category -->
-            <div class="input-group">
-                <label for="cat" class=""><?php _e('Select a Category: ', 'textdomain'); ?></label><br>
-                <select name="cat" class="form-control">
-                    <optgroup label="Categories">
-                        <option selected value="">All Categories</option>
-                        <?php
-                        foreach ($category as $cats) { ?>
-                        <option value="<?php echo $cats->slug?>"><?php echo $cats->name; ?></option>
-                        <?php
-                        }; ?>
-                    </optgroup>
-                </select>
-            </div>
-<!-- /Category -->
+			<div class="input-group date-search">
+				<div class="dateSearch">
+					<label for="startDate" class="sr-only">Range Start Date</label><?php _e('Search Between ', 'textdomain'); ?><br>
+					<input type="date" value="" name="startDate" id="startDate" placeholder="Start">
+				</div>
+				<div class="divider"> to </div>
+				<div class="dateSearch">
+					<label for="endDate" class="sr-only">Range End Date</label><br>
+					<input type="date" value="" name="endDate" id="endDate">
+				</div>
+				<div style="clear:both;"></div>
+			</div>
 
-<!-- File Type -->
-        <?php
-        if (class_exists('acf') && get_field('files')) { ?>
-            <div class="input-group">
-              <label for="fileType" class=""><?php _e('Select a Document Type: ', 'textdomain'); ?></label><br>
-              <select name="fileType" class="form-control">
-                  <option value="">All Types</option>
-                <?php
-                foreach ($fileType['choices'] as $k => $v) {?>
-                  <option value="<?php echo $k?>"><?php echo $v; ?></option>
-            <?php
-                };
-            ?>
-              </select>
-          </div>
-        <?php
-        };
-        ?>
-<!-- /File Type -->
-
-<!-- Start Date -->
-            <div class="input-group date-search">
-                <div class="dateSearch">
-                    <label for="startDate" class=""><?php _e('Search Between ', 'textdomain'); ?></label><br>
-                    <input type="date" value="" name="startDate" placeholder="Start">
-                </div>
-<!-- /Start Date -->
-                <div class="divider"> to </div>
-
-<!-- End Date -->
-                <div class="dateSearch">
-                    <label for="endDate" class=""><?php _e('  ', 'textdomain'); ?></label><br>
-                    <input type="date" value="" name="endDate">
-                </div>
-<!-- /input-group -->
-                <div style="clear:both;"></div>
-            </div>
-
-            <div class="input-group">
-                <span class="input-group-btn">
-                    <button class="btn" type="submit" value="Search" alt="Search">Search</button>
-                </span>
-            </div>
-<!-- /End Date -->
-        </form> <?php
-    } //widget end
+			<div class="input-group">
+				<span class="input-group-btn">
+					<button class="btn" type="submit" value="Search" alt="Search">Search</button>
+				</span>
+			</div>
+	</form> <?php
+	} //widget end
 } //class end
 
 // register widget
